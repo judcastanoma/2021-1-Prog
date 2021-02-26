4949fd9b424d1030419df7469cce130eda22a55a
judcastanoma <judcastanoma@unal.edu.co>
2021-02-26 16:59:31 +0000

    Mensaje

---
+++ hola.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //incluir la ilbreria de input ouput
+/*shfshdf
+*/
+//int argc char **arg para leer argumentos  de la linea de comando
+int main(int argc, char **argv) 
+{
+  std::cout << "hello world\n"; //caracter de nueva linea \n
+  return 0;
+}
+// comando rm para borrar
